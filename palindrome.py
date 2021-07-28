ac0e4f372ea9d5df7f0ced91157197367da7a62e
arturtondera <87845977+arturtondera@users.noreply.github.com>
2021-07-28 21:11:38 +0200

    add comments

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,10 @@
 def palindrome_check(word):
+    # dwie listy o tej samej zawartości
     list_of_letters1 = list(word)
     list_of_letters2 = list(word)
+    # odwracam kolejność jednej z nich:
     list_of_letters2.reverse()
+    # i powrównuję, co jest sprawdzeniem, czy wyraz jest palindromem:
     if list_of_letters1 == list_of_letters2:
         print("Tak")
     else:
